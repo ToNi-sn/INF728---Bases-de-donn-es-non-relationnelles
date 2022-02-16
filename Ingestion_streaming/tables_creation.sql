5136555ca77069148169d5a52f7edc64f620d2a4
ToNi-sn <80599627+ToNi-sn@users.noreply.github.com>
2022-02-16 11:44:21 +0100

    to .sql

--- Ingestion_streaming/tables_creation.sql
+++ Ingestion_streaming/tables_creation.sql
@@ -4,49 +4,49 @@ USE GDELT;
 
 CREATE TABLE nb_articles_events
 (
-    "globaleventid" int,    
-	  "day"          int,
-		"mentionid"   text,               
-    "action_geocountrycode"       text,               
-    "mentiondoctranslationalinfo"       text,                    
-		PRIMARY KEY (("day","action_geocountrycode","mentiondoctranslationalinfo"),"globaleventid","mentionid")
+	"globaleventid" int,    
+	"day" int,
+	"mentionid"   text,               
+	"action_geocountrycode" text,               
+	"mentiondoctranslationalinfo" text,                    
+	PRIMARY KEY (("day","action_geocountrycode","mentiondoctranslationalinfo"),"globaleventid","mentionid")
 );
 
 
 CREATE TABLE countries_events
 (
-    "globaleventid"         int,
-		"day"                     int,
-		"month"                   int,
-		"year"                    int,
-    "nummentions"             int,
-    "action_geocountrycode" text,
-    PRIMARY KEY (("action_geocountrycode"),"month","day","globaleventid")
+	"globaleventid" int,
+	"day" int,
+	"month" int,
+	"year" int,
+	"nummentions" int,
+	"action_geocountrycode" text,
+	PRIMARY KEY (("action_geocountrycode"),"month","day","globaleventid")
 );
 
 
 CREATE TABLE data_source
 (
-    "day"       int,
-		"month"     int,
-    "sourcecommonname" text,
-	  "documentidentifier" text,
-    "themes"      set<text>,
-    "persons"    set<text>,
-    "locations"  set<text>,
-    "tone"       float,
-    PRIMARY KEY (("sourcecommonname"),"day","month","documentidentifier")
+	"day" int,
+	"month" int,
+	"sourcecommonname" text,
+	"documentidentifier" text,
+	"themes" text,
+	"persons" text,
+	"locations" text,
+	"tone" float,
+	PRIMARY KEY (("sourcecommonname"),"day","month","documentidentifier")
 );
 
 
 CREATE TABLE relationship
 (
-    "sourceurl"        text,
-    "day"               int,
-		"month"             int,
-		"averagetone"      float,
-		"actor1_geocountrycode"   text,
-    "actor2_geocountrycode"   text,
-    "themes" set<text>,
- PRIMARY KEY (("actor1_geocountrycode","actor2_geocountrycode"),"month","day","sourceurl")
+	"sourceurl" text,
+	"day" int,
+	"month" int,
+	"averagetone" float,
+	"actor1_geocountrycode" text,
+	"actor2_geocountrycode" text,
+	"themes" text,
+	PRIMARY KEY (("actor1_geocountrycode","actor2_geocountrycode"),"month","day","sourceurl")
 );
